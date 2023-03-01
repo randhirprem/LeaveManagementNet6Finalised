3d73fdf07f1522856278b32078214220f14fa162
maria <maria@DESKTOP-25TCAUA>
2023-03-01 12:06:23 +0800

    Add project files.

---
+++ LeaveManagement.Web/Controllers/LeaveTypesController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LeaveManagement.Data;
+using AutoMapper;
+using LeaveManagement.Common.Models;
+using LeaveManagement.Application.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using LeaveManagement.Common.Constants;
+
+namespace LeaveManagement.Web.Controllers
+{
+    [Authorize(Roles = Roles.Administrator)]
+
+    public class LeaveTypesController : Controller
+    {
+        private readonly ILeaveTypeRepository leaveTypeRepository;
+        private readonly IMapper mapper;
+        private readonly ILeaveAllocationRepository leaveAllocationRepository;
+
+        public LeaveTypesController(ILeaveTypeRepository leaveTypeRepository
+            , IMapper mapper
+            , ILeaveAllocationRepository leaveAllocationRepository)
+        {
+            this.leaveTypeRepository = leaveTypeRepository;
+            this.mapper = mapper;
+            this.leaveAllocationRepository = leaveAllocationRepository;
+        }
+
+        // GET: LeaveTypes
+        public async Task<IActionResult> Index()
+        {
+            var leaveTypes = mapper.Map<List<LeaveTypeVM>>(await leaveTypeRepository.GetAllAsync());
+            return View(leaveTypes); 
+        }
+
+        // GET: LeaveTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            var leaveType = await leaveTypeRepository.GetAsync(id);
+            if (leaveType == null)
+            {
+                return NotFound();
+            }
+
+            var leaveTypeVM = mapper.Map<LeaveTypeVM>(leaveType);
+            return View(leaveTypeVM);
+        }
+
+        // GET: LeaveTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeaveTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(LeaveTypeVM leaveTypeVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var leaveType = mapper.Map<LeaveType>(leaveTypeVM);
+                await leaveTypeRepository.AddAsync(leaveType);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(leaveTypeVM);
+        }
+
+
+        // GET: LeaveTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var leaveType = await leaveTypeRepository.GetAsync(id);
+            if (leaveType == null)
+            {
+                return NotFound();
+            }
+
+            var leaveTypeVM = mapper.Map<LeaveTypeVM>(leaveType);
+            return View(leaveTypeVM);
+        }
+
+        // POST: LeaveTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, LeaveTypeVM leaveTypeVM)
+        {
+            if (id != leaveTypeVM.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var leaveType = mapper.Map<LeaveType>(leaveTypeVM);
+                    await leaveTypeRepository.UpdateAsync(leaveType);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await leaveTypeRepository.Exists(leaveTypeVM.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(leaveTypeVM);
+        }
+
+        // POST: LeaveTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await leaveTypeRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AllocateLeave(int id)
+        {
+            await leaveAllocationRepository.LeaveAllocation(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ LeaveManagement.Web/Program.cs
@@ -0,0 +1,71 @@
+using LeaveManagement.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using LeaveManagement.Application.Configurations;
+using LeaveManagement.Application.Contracts;
+using LeaveManagement.Application.Repositories;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using LeaveManagement.Web;
+using LeaveManagement.Web.Services;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Host.UseSerilog((ctx, lc) => 
+    lc.WriteTo.Console()
+    .ReadFrom.Configuration(ctx.Configuration));
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddTransient<IEmailSender>(s => new EmailSender("localhost", 25, "no-reply@leavemanagement.com"));
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+builder.Services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+builder.Services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseSerilogRequestLogging();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
